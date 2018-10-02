2d4ace1a740b684dce5d9d5868eebb28165e6f0f
Al <al-mac@MacBook-Pro-de-Al.local>
2018-10-02 05:08:48 -0600

    Added a general transmitter to the connection event

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Receiving data from the server');
 
-    socket.emit('createMessage',{
-        from: 'boshka',
-        text: 'this is the super duper Boshka :)'
-    });
+
 });
 
 socket.on('disconnect', function(){
