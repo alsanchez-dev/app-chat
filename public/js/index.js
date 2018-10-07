05c2149e3567107e4b5a000b46720228901844b2
Al <al-mac@MacBook-Pro-de-Al.local>
2018-10-06 20:25:09 -0600

    Added jQuery auto scroll for new messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom (){
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+
+    }
+}
+
 
 socket.on('connect', function(){
     console.log('Receiving data from the server');
@@ -22,7 +39,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // console.log('newMessage', message);
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -39,6 +56,7 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank"> My current location </a>');
     //
